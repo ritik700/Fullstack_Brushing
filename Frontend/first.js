79f4bf5375506ce5414b9768e546cd38462380a8
ritik700 <ritikagarwal700@gmail.com>
2026-05-21 11:59:23 -0700

    brushing the concept of loops

--- Frontend/first.js
+++ Frontend/first.js
@@ -1,1 +1,17 @@
 console.log("hello");
+
+//for of loop practice
+let varStr = "Congratulations";
+
+for (let val of varStr){
+    console.log(val);
+}
+
+//for in loop practice
+let str = "Congratulations";
+
+
+for (let key in str){
+    console.log(key);
+    console.log(str[key]);
+}
